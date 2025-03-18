19c5a69472c80a5222e6ca361fa87c7db378faf4
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-03-18 21:22:58 +0530

    created new component AuctionStatus and entered initial code

--- src/component/dashboard/buyer/AuctionStatus.jsx
+++ src/component/dashboard/buyer/AuctionStatus.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Status() {
-  return (
-    <div>
-      <h1>Status</h1>
-    </div>
-  );
-}
+const AuctionStatus = (userRole, userId) => {
+  const navigate = useNavigate();
+  const [buyerInfo, setBuyerInfo] = useState(null);
+  const [hasAlertShown, setHasAlertShown] = usestate(false);
+
+  // fetch buyer details from backend
+  useEffect(() => {
+    const fetchBuyerInfo = async () => {
+      try {
+        const response = await axios
+          .get
+          // put data fetching api of user
+          ();
+        setBuyerInfo(response.data);
+      } catch (error) {
+        console.error("Failed to fetch buyer info : ", error);
+      }
+    };
+    fetchBuyerInfo();
+  }, [userId]);
+};
+
+export default AuctionStatus;
