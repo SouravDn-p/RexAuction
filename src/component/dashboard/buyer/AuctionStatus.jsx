8eb4083f5e0c7e8ef6dc4c896da463f97637a49b
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-03-19 15:17:53 +0530

    created auction status element and entered initial variables

--- src/component/dashboard/buyer/AuctionStatus.jsx
+++ src/component/dashboard/buyer/AuctionStatus.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { motion } from "framer-motion";
+import axios from "axios";
 
-export default function AuctionStatus() {
-  return (
-    <div>
-      <h1>Status</h1>
-      <button>Payment</button>
-    </div>
-  );
-}
+const BuyerStatus = ({ userRole, userId }) => {
+  const navigate = useNavigate();
+  const [buyerInfo, setBuyerInfo] = useState(null);
+  const [hasAlertShown, setHasAlertShown] = useState(false);
+
+  // Fetch buyer details from backend
+  useEffect(() => {
+    const fetchBuyerInfo = async () => {
+      try {
+        const response = await axios
+          .get
+          //   user data fetching api
+          ();
+        setBuyerInfo(response.data);
+      } catch (error) {
+        console.error("Failed to fetch buyer info:", error);
+      }
+    };
