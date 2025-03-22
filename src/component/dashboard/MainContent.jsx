7229a4a2e251e034c8764dae26fa09b702de75fa
Sourav-Dn-p <sdsouravdebnath26@gmail.com>
2025-03-23 00:06:54 +0600

    Dashboard navbar solved

--- src/component/dashboard/MainContent.jsx
+++ src/component/dashboard/MainContent.jsx
@@ -36,9 +36,9 @@ const MainContent = () => {
 
       {/* Content Container */}
       <div className="container mx-auto">
-        <div>
+        {/* <div>
           <DashboardNavbar />
-        </div>
+        </div> */}
 
         {/* Main Dashboard Content */}
         <div className="bg-white rounded-lg p-5 overflow-x-auto flex-grow">
