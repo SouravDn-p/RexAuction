bf72b32e259c49631741244bdb04470cd00a3d7a
Sourav-Dn-p <sdsouravdebnath26@gmail.com>
2025-04-18 21:04:27 +0600

    feedback page added second time

--- src/component/dashboard/admin/feedbacks/FeedbackDisplay.jsx
+++ src/component/dashboard/admin/feedbacks/FeedbackDisplay.jsx
@@ -79,8 +79,6 @@ const FeedbackDisplay = () => {
       ));
   };
 
-  
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-50 to-white dark:from-gray-900 dark:to-gray-800 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
